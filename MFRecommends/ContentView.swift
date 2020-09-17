942e3a955357fdcd19a812d784f829a83881c0df
tyrone <544137553@qq.com>
2020-09-17 21:54:41 +0800

    Initial Commit

---
+++ MFRecommends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MFRecommends
+//
+//  Created by 唐桂富 on 2020/9/17.
+//  Copyright © 2020 Tyrone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MFRecommendsTests/MFRecommendsTests.swift
@@ -0,0 +1,34 @@
+//
+//  MFRecommendsTests.swift
+//  MFRecommendsTests
+//
+//  Created by 唐桂富 on 2020/9/17.
+//  Copyright © 2020 Tyrone. All rights reserved.
+//
+
+import XCTest
+@testable import MFRecommends
+
+class MFRecommendsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
